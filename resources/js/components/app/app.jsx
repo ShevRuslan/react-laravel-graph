97341877c2020127d861e5f63ab0042e89503636
sasa02sasa <sasa02sasa@mail.ru>
2019-05-02 22:28:47 +0600

    Create header

--- resources/js/components/app/app.jsx
+++ resources/js/components/app/app.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import Home from '../home'
+import Home from '../home';
+import Header from '../header';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import ReactDOM from 'react-dom';
 export default class App extends Component {
     render() {
         return (
-            <Router>
-                <Route exact path='/' component={Home}></Route>
-            </Router>
+            <div>
+                <Header/>
+                <Router>
+                    <Route exact path='/' component={Home}></Route>
+                </Router>
+            </div>
         );  
     }
 }
