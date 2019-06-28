4ecd727463b1c19721c1adeb27bfaf27251b3af1
sasa02sasa <sasa02sasa@mail.ru>
2019-06-28 12:04:39 +0600

    Create footer

--- resources/js/components/app/app.jsx
+++ resources/js/components/app/app.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from '../home';
 import Header from '../header';
+import Footer from '../footer';
 import {Route, Switch} from 'react-router-dom'
 export default class App extends Component {
     render() {
@@ -10,6 +11,7 @@ export default class App extends Component {
                 <Switch>
                     <Route exact path='/' component={Home}></Route>
                 </Switch>
+                <Footer/>
             </div>
         );  
     }
